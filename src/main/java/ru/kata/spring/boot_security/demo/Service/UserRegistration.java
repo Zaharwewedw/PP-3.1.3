0404a053ecad707e95ef6bfb0db1ae4b6a06f937
Zahar-Write-Code <tolst.0.2.5.0.2.5@gmail.com>
2023-06-25 12:53:34 +0300

    add BCryptPasswordEncoder

--- src/main/java/ru/kata/spring/boot_security/demo/Service/UserRegistration.java
+++ src/main/java/ru/kata/spring/boot_security/demo/Service/UserRegistration.java
@@ -1,7 +1,7 @@
 package ru.kata.spring.boot_security.demo.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
@@ -11,14 +11,17 @@ import ru.kata.spring.boot_security.demo.model.User;
 public class UserRegistration {
 
     private final UserDao userDao;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserRegistration(UserDao userDao) {
+    public UserRegistration(UserDao userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(User user){
-        userDao.saveUser(user.getUsNa(), user.getPass(), user.getEmail(), user.getAge(), user.getName());
+        userDao.saveUser(user.getUsNa(), passwordEncoder.encode(user.getPass()),
+                user.getEmail(), user.getAge(), user.getName());
     }
 }
